bf44c0f1ca9f252407f891db456aa71171d0aeb6
nhuseynli7813 <nhuseynli7813@ada.edu.az>
2021-12-30 20:44:49 +0400

    FileParser updated

--- FileParser.java
+++ FileParser.java
@@ -1,24 +1,23 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class FileParser{
     public static ArrayList<ArrayList<String>> readCsvFile(String fileName){
         File inpFile = new File(fileName);
         ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
-
+        for(int i = 0; i < 16;i++){
+            data.add(new ArrayList<String>());
+        }
         try(BufferedReader br = new BufferedReader(new FileReader(inpFile))) {
             br.readLine();
 			for(String line; (line = br.readLine()) != null; ){
-				ArrayList<String> row = new ArrayList<String>();
-                String[] parsed = line.split(",");
+                String[] parsed = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)",-1);
                 for(int i =0 ; i < parsed.length; i++){
-                    parsed[i] = parsed[i].trim();
+                    data.get(i).add(parsed[i].trim());
                 }
-                Collections.addAll(row, parsed);
-                data.add(row);
 			}
 		}catch (Exception e){
+            System.out.println("Error occured while reading and parsing csv file!\nClass: FileParser\nMethod: readCsvFile(String fileName)");
         }
 
         return data;
