e544743d6b0368844ef28efccaf9ac712aaab4ab
nhuseynli7813 <nhuseynli7813@ada.edu.az>
2021-12-31 15:20:18 +0400

    File removed

--- Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Main{
-    public static void main(String[] args) {
-        ArrayList<Crash> crashes = Utils.readFromFile("./Large_Passenger_Plane_Crashes_1933_to_2009.csv");
-        ReportManager.PrintCrashes(crashes);
-    }
-}
--- ReportManager.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public abstract class ReportManager {
-    static void PrintCrashes(List<Crash> crashes){
-        for (Crash crash : crashes) {
-            System.out.println(crash.toString());
-        }
-    }
-}
